6bbf342bfb0c04106d473f310dc322872e41a1a6
madan-stha <madan@scieverinc.com>
2024-11-14 11:51:56 +0545

    added easy ocr api

--- services/file_service.py
+++ services/file_service.py
@@ -1,11 +1,13 @@
 import os
 from pathlib import Path
-from paddleocr import PaddleOCR
+# from paddleocr import PaddleOCR
 from fastapi.responses import FileResponse
 from typing import List
-from config import UPLOAD_FOLDER
 import cv2
 import numpy as np
+from easyocr import Reader
+
+from config import UPLOAD_FOLDER
 
 
 class OCRResponse:
@@ -16,7 +18,48 @@ class OCRResponse:
         self.highlighted_file_link = highlighted_file_link
 
 
-ocr = PaddleOCR(use_angle_cls=True, lang="en", use_gpu=True)
+# ocr = PaddleOCR(use_angle_cls=True, lang="en", use_gpu=True)
+reader = Reader(['en'], gpu=False)
+
+
+# def save_file(file):
+#     file_name = file.filename
+#     file_path = os.path.join(UPLOAD_FOLDER, file_name)
+
+#     with open(file_path, 'wb') as f:
+#         f.write(file.file.read())
+
+#     result = ocr.ocr(file_path, cls=True)
+
+#     if not result or not result[0]:
+#         return OCRResponse(text=[], confidence=[], file_link=file_path, highlighted_file_link="")
+
+#     texts = []
+#     confidences = []
+#     highlighted_image_path = os.path.join(
+#         UPLOAD_FOLDER, f"highlighted_{file_name}")
+
+#     image = cv2.imread(file_path)
+
+#     for line in result[0]:
+#         text, confidence = line[1]
+#         texts.append(text)
+#         confidences.append(float(confidence))
+
+#         bbox = line[0]
+#         pts = [(int(point[0]), int(point[1])) for point in bbox]
+
+#         cv2.polylines(image, [np.array(pts)], isClosed=True,
+#                       color=(0, 255, 0), thickness=2)
+
+#     cv2.imwrite(highlighted_image_path, image)
+
+#     return OCRResponse(
+#         text=texts,
+#         confidence=confidences,
+#         file_link=file_path,
+#         highlighted_file_link=highlighted_image_path
+#     )
 
 
 def save_file(file):
@@ -26,9 +69,9 @@ def save_file(file):
     with open(file_path, 'wb') as f:
         f.write(file.file.read())
 
-    result = ocr.ocr(file_path, cls=True)
+    result = reader.readtext(file_path, detail=1)
 
-    if not result or not result[0]:
+    if not result:
         return OCRResponse(text=[], confidence=[], file_link=file_path, highlighted_file_link="")
 
     texts = []
@@ -38,12 +81,10 @@ def save_file(file):
 
     image = cv2.imread(file_path)
 
-    for line in result[0]:
-        text, confidence = line[1]
+    for bbox, text, confidence in result:
         texts.append(text)
         confidences.append(float(confidence))
 
-        bbox = line[0]
         pts = [(int(point[0]), int(point[1])) for point in bbox]
 
         cv2.polylines(image, [np.array(pts)], isClosed=True,
